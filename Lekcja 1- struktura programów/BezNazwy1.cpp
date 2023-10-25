5c8b6325a1baff07f43f0b65982a6a1020366187
Paandusia <asuna_3@w.pl>
2023-10-25 14:11:07 +0200

    Lekcja1

---
+++ Lekcja 1- struktura programów/BezNazwy1.cpp
@@ -0,0 +1,27 @@
+ #include <iostream>
+ using namespace std;
+ 
+ int main(){
+ 	
+ 	
+ 	short liczba = 2;
+ 	float zmiennoprzecinkowa = 3.532;
+ 	string tekst = "tekst";
+ 	char zank ='A';
+ 	bool znakowy  = false;
+ 	
+ 	
+ 	
+ 	cout <<"Hello World" << endl;
+ 	cout << "Liczba calkowita: " << liczba;
+ 	cout <<endl;
+ 	
+ 	
+ 	string jakos; 
+ 	cout << "Podaj jakis wyraz: ";
+ 	cin >> jakos;
+ 	cout << "Wprowadziles wartosc: " << jakos;
+ 	
+ 	return 0;
+ }
+ 
