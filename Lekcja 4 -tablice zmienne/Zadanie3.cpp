9d6bff4bcb39e2316a09a12be0b300699e23eb3d
Paandusia <asuna_3@w.pl>
2023-12-07 15:00:38 +0100

    kontunuacja zadan

--- Lekcja 4 -tablice zmienne/Zadanie3.cpp
+++ Lekcja 4 -tablice zmienne/Zadanie3.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
 	
 	int tablica [5];
 	
-	 
-	cout << "Wprowadz liczby: ";
 	
 	for(int i = 0; i < 5; i++){
 		cin >>  tablica[i];
@@ -15,10 +13,13 @@ int main() {
 	
 	
 	for(int i = 0; i <5; i++){
-	 if(tablica[i] /2 == 0) cout << "Liczby parzyste: ";
-	}
+		cout << tablica [i];
 	
 	
+	
+	
+}
 
-	return 0;
+return 0;
+
 }
