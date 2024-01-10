cbea17d41969033bc371619bc06f98c1a6c3b3e5
Paandusia <asuna_3@w.pl>
2024-01-10 14:06:54 +0100

    Funkcje zadanie 2

---
+++ Lekcja 6 - Funkcje/Zadanie 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+void Przywitanie(string imie, string nazwisko);
+
+int main (){
+	string imie, nazwisko;
+	cout << "Podaj swoje imie: ";
+	cin >> imie;
+	cout << "Podaj swoje nazwisko: ";
+	cin >> nazwisko;
+	Przywitanie(imie, nazwisko);
+	
+}
+
+void Przywitanie(string imie, string nazwisko){
+	cout << "Witaj " << imie << " " << nazwisko;
+}
