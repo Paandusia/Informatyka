941b567e0025863d46d595eb02e1e5741a0c56b1
Paandusia <asuna_3@w.pl>
2024-02-05 13:14:22 +0100

    Zadanie 1 - przeciaznie funkcji

---
+++ Lekcja 8 - przeciazanie funkcji/Przeciazanie funkcji - zadanie 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int Suma(int a, int b){
+	return a+b;
+}
+
+float Suma(float c, float d){
+	return c+d;
+}
+
+int main(){
+	int a,b;
+	float c,d;
+	char wybor;
+	
+	cout << "1- Liczby calkowite" << endl;
+	cout << "2- Liczby zmiennoprzecinkowe" << endl;
+	cin >> wybor;
+	
+	switch(wybor){
+		case '1':
+			cout << "Podaj dwie liczy calkowite: "; cin>>a >>b;
+			cout << "Suma liczb calkowitych: " <<Suma(a,b) <<endl;
+			break;
+			
+		case '2':
+			cout << "Podaj dwie liczy zmiennoprzecinkowe: "; cin>>c >>d;
+			cout << "Suma liczb zmiennoprzecinkowych: " <<Suma(c,d) <<endl;
+			break;
+		
+	
+	}
+	return 0;
+		
+}
