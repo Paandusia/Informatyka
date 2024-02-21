b2ecd1447415a2e2cdc4b2fefecef34aa0b34759
Paandusia <asuna_3@w.pl>
2024-02-21 14:08:14 +0100

    Wskazniki - zadanie4

--- Lekcja 9 - Wskazniki/Wskazniki - zadanie 4.cpp
+++ Lekcja 9 - Wskazniki/Wskazniki - zadanie 4.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 
 
+void a (int tablica[], int b){
+	cout << "Podaj 5 liczb, ktore nalezy zsumowac: " << b << endl;
+	
+}
 
 int main(){
-	
-	cout << "Podaj 5 liczb, ktore nalezy zsumowac: " << *tablica << endl;
-	
 	int tablica[5];
-
-
-
+	for(int j = 0 ; j < 5; j++){
+		cout << "Podaj 5 liczb, ktore nalezy zsumowac: " << j;
+	}
 	
+
+
 	return 0;
 }
+
+
