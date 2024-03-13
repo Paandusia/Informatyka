812d64761a5279282b27673b0f2a8e41d87e35a6
Paandusia <asuna_3@w.pl>
2024-03-13 14:10:16 +0100

    Zadanie 2- struktury danych

--- Lekcja 11 - Struktury danych/Zadanie 1.cpp
+++ Lekcja 11 - Struktury danych/Zadanie 1.cpp
@@ -1,68 +1,85 @@
 #include <iostream>
-usingnamespace std;
+using namespace std;
 
 //wprowadzenie danych nazwa, waga(gram), wiek (tygodnie),
 
 //struct_zwierzeta_DDT{
-	string nazwa;
-	atring waga (gram);
-	string wiek (tygodnie);
-};
+//	string nazwa;
+//	string waga (gram);
+//	string wiek (tygodnie);
+//};
 
  //int main(){
-	zwierzeta_DDT krowa = {
+	//zwierzeta_DDT krowa = {
 	
-		"Krowa",
-		10,
-		20,
-	};
+	//	"Krowa",
+	//	10,
+	//	20,
+	//};
 	
-	zwierzeta_DDT koza = {
+	//zwierzeta_DDT koza = {
 	
-		"Koza",
-		10.6,
-		15,
-	};
+	//	"Koza",
+	//	10.6,
+	//	15,
+	//};
 	
-	zwierzeta_DDT kura = {
+	//zwierzeta_DDT kura = {
 	
-		"Kura",
-		2.8,
-		4,
-	};
+	//	"Kura",
+	//	2.8,
+	//	4,
+	//};
 	
-	zwierzeta_DDT pies = {
+	//zwierzeta_DDT pies = {
 	
-		"Pies",
-		13.4,
-		35,
-	};
+	//	"Pies",
+	//	13.4,
+	//	35,
+	//};
 	
-	zwierzeta_DDT swinia = {
+	//zwierzeta_DDT swinia = {
 	
-		"Swinia",
-		12.75,
-		8
-	};
-	
-	
-	
+	//	"Swinia",
+	//	12.75,
+	//	8
+	//};
 	
 	
 //}}
 
 
 
-
-struct DDT{
+struct lol {
 	string nazwa;
 	string waga;
 	string wiek;
 };
 
-int mian(){
-	DDT Zwierzeta[5];
-	cout <<
+int main(){
+	 lol Zwierzeta[5];
+	cout <<"Wprowadz dane zwierzat: " << endl;
+	for (int a = 0; a < 5; a++){
+		cout<< "Nazwa: ";
+		cin >> Zwierzeta[a].nazwa;
+		cout << "Waga(gram): ";
+		cin >> Zwierzeta[a].waga;
+		cout << "Wiek(tygodnie): ";
+		cin >> Zwierzeta[a].wiek;	
+	}
+	
+	
+	cout << endl; 
+	cout << "Wprowadzone zwierzeta: " << endl;
+	for (int a = 0; a < 5; a++){
+		cout << "Zwierze nr " << a + 1
+		<< "\tNazwa: " << Zwierzeta[a].nazwa
+		<< "\tWaga: " << Zwierzeta[a].waga
+		<< "\tWiek: " << Zwierzeta[a].wiek
+		<< endl;
+	}
+	
+	cin.get();
+	return(0);
 }
 
-};
