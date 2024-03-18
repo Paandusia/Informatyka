57a6d143177bf8a4d0837ead4f2d99256a312170
Paandusia <asuna_3@w.pl>
2024-03-18 14:09:33 +0100

    zadanie 2 struktury

--- Lekcja 11 - Struktury danych/Zadanie 2.cpp
+++ Lekcja 11 - Struktury danych/Zadanie 2.cpp
@@ -10,9 +10,12 @@ struct kot{
 	float cena;
 };
 
+//void szukanieRok (rok){
+	
+//}
+
 int main(){
-	kot Ksiegarnia [7];
-	{
+	kot Ksiegarnia [7]{
 		{ "Tytul1", "autor1", 1925, "Helion1", 23.54 },
     	{ "Tytul2", "autor2", 1926, "Helion2", 24.54 },
     	{ "Tytul3", "autor1", 1927, "Helion3", 25.54 },
@@ -22,4 +25,40 @@ int main(){
     	{ "Tytul7", "autor1", 1931, "Helion1", 29.54 },
 	};
 	
+	cout << "\tWitaj w ksiegarnii" << endl;;
+	//cout << "1. Rok wydania" << ;
+	//cout << "2. Cena" << ;
+	//cout << "3. Wydawnictwo" << ;
+	//cout << "0. Wyjscie" << ;
+	
+	
+	cout << "\tPo jakiej kategorii chcial bys wyszukac ksiazke: " << endl;
+		for (int nwm=0; nwm < 7; nwm++){
+			cout << "1. Rok wydania" << Ksiegarnia[nwm].rok;
+			cout << "2. Cena" << Ksiegarnia[nwm].cena;
+			cout << "3. Wydawnictwo" <<  Ksiegarnia[nwm].wydawnictwo;
+		//	cout << "0. Wyjscie" << Ksiegarnia[nwm].;
+	int wybor;
+	switch (wybor){
+		case 1:
+			cout << "\t1. Rok wydania";
+			cin >> Ksiegarnia[nwm].rok;
+			break;
+		case 2:
+			cout << "\t2. Cena";
+			cin >> Ksiegarnia[nwm].cena;    
+			break;
+		case 3:
+			cout << "\t3. Wydawnictwo";
+			cin >> Ksiegarnia[nwm].wydawnictwo;
+			break;
+		//case 0:
+		//	cout << "0. Wyjscie";
+		//	cin >>
+		}
+	}
+	
+	
+	cin.get();
+	return 0;
 }
